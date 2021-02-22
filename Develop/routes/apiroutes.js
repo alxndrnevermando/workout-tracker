0c6e0f0ead08d774595b5678f163abc3f485515d
Armando Contreras <armandocontreras62596@gmail.com>
2021-02-21 17:14:50 -0800

    creating my post route

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,20 +1,20 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(workouts => res.json(workouts))
+      .catch(err => res.json(err));
+  });
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(newWorkout => {
+      res.json(newWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
-});
-
-router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .then(workouts => res.json(workouts))
-    .catch(err => res.json(err));
 });
 
 module.exports = router;
