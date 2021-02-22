3c4bb9779e58034204b874fd03fbdba5a7b7f3ed
Armando Contreras <armandocontreras62596@gmail.com>
2021-02-21 17:19:00 -0800

    created my put route, this is for updating saved workouts if the user wants to update info

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -7,6 +7,16 @@ router.get("/api/workouts", (req, res) => {
       .catch(err => res.json(err));
   });
 
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        {$push: {exercises: req.body}},
+        {new: true}
+    )
+        .then(workouts => res.json(workouts))
+        .catch(err => res.json(err));
+});
+
 router.post("/api/workouts", (req, res) => {
   Workout.create({})
     .then(newWorkout => {
