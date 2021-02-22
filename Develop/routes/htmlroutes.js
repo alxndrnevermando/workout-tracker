0b07c2a13b43568456268a123fcb0ff29e639088
Armando Contreras <armandocontreras62596@gmail.com>
2021-02-21 16:18:14 -0800

    included a get route to my index page here instead of in my server file

---
+++ Develop/routes/htmlroutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  
+router.get("/exercise", (req, res) => {
+  console.log("Checking for connection");
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  console.log("Checking for connection");
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
