2f176bb2db54bdc9a271478e1ee3fe1b270bcfb5
Armando Contreras <armandocontreras62596@gmail.com>
2021-02-21 18:17:10 -0800

    had to fix a few things. Needed to require path, as I still have all of the models, static files, and routes under the Develop folder. Had issues getting my css to show up, telling me there was a problem with my path. Then, I actually use my routes and pass them to my express app.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 //Require routes
+const apiRoutes = require("./Develop/routes/apiroutes")
 const htmlRoutes = require("./Develop/routes/htmlroutes")
 
 const PORT = process.env.PORT || 3000;
@@ -14,12 +16,13 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, './Develop/public')));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
 //Routes go here
 
+app.use(apiRoutes);
 app.use(htmlRoutes);
 
 app.listen(PORT, () => console.log("listening on port: ", PORT));
